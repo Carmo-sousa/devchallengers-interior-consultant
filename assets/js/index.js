91a950fb8de3a46d3906303bac3fa05fdeea0061
Rômulo do Carmo Sousa <carmosousa20@outlook.com>
2020-11-06 15:23:26 -0400

    Menu responsivo finalizado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,16 @@
-let itemLink = document.querySelectorAll('.item__link');
+const btnMenuMobile = document.querySelector(".mobile-button-menu");
+const btnMenu = document.querySelector(".button-menu");
 
-console.log(itemLink);
+btnMenuMobile.addEventListener("click", () => toggleClass());
+btnMenu.addEventListener("click", () => toggleClass());
+
+function toggleClass() {
+    let mobileMenuEl = document.querySelector(".mobile-nav");
+    let open = mobileMenuEl.classList.contains("active") ? true : false;
+
+    if(open) {
+        mobileMenuEl.classList.remove("active");
+    } else {
+        mobileMenuEl.classList.add("active");
+    }
+}
